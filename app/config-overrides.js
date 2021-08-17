8daa21e0fc09055705f302519692abcb1af55eed
jeroenvdmeer <1658477+jeroenvdmeer@users.noreply.github.com>
2021-08-17 18:48:24 +0200

    Made easier to find password for level 3 in JS

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -1,36 +1,5 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-
 module.exports = function override(config, env) {
-    /*config.plugins[0] = new HtmlWebpackPlugin({
-        inject: true,
-        template: paths.appHtml,
-        minify: {
-            removeComments: true,
-            collapseWhitespace: true,
-            removeRedundantAttributes: true,
-            useShortDoctype: true,
-            removeEmptyAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            keepClosingSlash: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true,
-        }
-    })*/
-    /*config.plugins[0].options.minify = {
-        removeComments: false,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-    }*/
     config.plugins[0].options.minify.removeComments = false
-    console.log(config.plugins[0].options)
-
+    config.optimization.minimize = false
     return config
 }
--- app/src/utils/levels.js
+++ app/src/utils/levels.js
@@ -1,6 +1,7 @@
+const LEVEL3_PASSWORD = "hdBue8ahA"
 const LEVELS = {
     en: [
-            { // Level 1
+            {
                 intro: "Lets start easy. What's the first combination any hacker would try?",
                 hints: [
                     "You can see the username already in the placeholder text when you leave the Username field empty, so you're already halfway there!",
@@ -9,7 +10,7 @@ const LEVELS = {
                 ],
                 success: "Good job! Trying the combination admin/admin is standard check any hacker would start with. This is because many software and devices are shipped with 'admin' as the default value for both the username and the password. Many times administrators forget to change these default values after installation, leaving the system open to unauthorised access."
             },
-            { // Level 2
+            {
                 intro: "Lets get technical. Front-ends of web applications consist of HTML, CSS and JavaScript code. Your web browser has developer tools available to inspect this code. Can you find a hint to the password of this level?",
                 hints: [
                     "To open the developer tools of your browser, hit the keyboard shortcut Ctrl + Shift + i (Cmd + Shift + i if you're on macOS).",
@@ -18,17 +19,17 @@ const LEVELS = {
                 ],
                 success: "Awesome! Although it's very unlikely you'll find passwords hidden in HTML code, inspecting front-end code (especially the JavaScript) is likely to provide clues of potential weaknesses and backdoors."
             },
-            { // Level 3
+            {
+                password: LEVEL3_PASSWORD,
                 intro: "What other clues can you find in the frontend code?",
                 hints: [
                     "In the previous level you found a clue in the HTML code. Have you tried searching the JavaScript code?",
                     "The Network tab of the developer tools has a very powerful Search function to search in all frontend code. You might need to refresh the page before the search works.",
                     "The magnifying glass opens the search function in the Network tab. Try to think of some clever keywords to search for. And carefully analyze the results."
                 ],
-                success: "Winning! Although finding clues in HTML code is rare, finding clues or even passwords and API keys in JavaScript code is actually not very uncommon. The combination of the developer tools and some creativity can get you a long way.",
-                password: "hdBue8ahA"
+                success: "Winning! Although finding clues in HTML code is rare, finding clues or even passwords and API keys in JavaScript code is actually not very uncommon. The combination of the developer tools and some creativity can get you a long way."
             },
-            { // Level 4
+            {
                 intro: <img src="/level4/banana.gif" alt="Banana" />,
                 hints: [
                     "Have you inspected all parts of the banana?",
@@ -37,7 +38,7 @@ const LEVELS = {
                 ],
                 success: "Super! It's not uncommon to find files and folders unprotected. Just because there's no link pointing to that location, does not mean no one can find it. Guessing common filenames and cleverly using search engines can get you a long way."
             },
-            { // Level 5
+            {
                 intro: "Lets turn our focus to the API. Sometimes it can give us some clues as well.",
                 hints: [
                     "Have you noticed anything different in the error messages?",
@@ -46,7 +47,7 @@ const LEVELS = {
                 ],
                 success: "Very good! That weird text was actually a hash. An MD5 hash to be precise. MD5 is an old algorithm that unfortunately is still used to 'protect' passwords. But as you found out, it can be easy to find the original values of MD5 hashes."
             },
-            { // Level 6
+            {
                 intro: "Again, lets take a look at the API. Can you find some clues now?",
                 hints: [
                     "In the previous level we saw the full details of the error message on our screen. Could it be some information is filtered?",
@@ -55,7 +56,7 @@ const LEVELS = {
                 ],
                 success: "Boom! It's not uncommon for APIs to share many details. Although they might not all appear on your screen, when you know your way with your web browser's developer tools, you can learn a lot about the API and its backend."
             },
-            {// Level 7
+            {
                 intro: "The grand finale! The password for this level is very strong. Can you find another way to login?",
                 hints: [
                     "Only one hint this time. Have you figured out how each time you refresh the page you don't get redirected to level 1?"
