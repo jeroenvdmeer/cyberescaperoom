5729706f40f167fb6bf01aa65ecb88841eb818b5
jeroenvdmeer <1658477+jeroenvdmeer@users.noreply.github.com>
2026-05-29 14:40:26 +0200

    feat: migrate Success.js to react-icons and Chakra v3

--- app/src/components/Success.js
+++ app/src/components/Success.js
@@ -1,36 +1,39 @@
 import {
-    Box,
-    Button,
-    Text,
-    Link,
-    List,
-    ListItem,
-    ListIcon
+  Box,
+  Button,
+  Text,
+  Link,
+  List,
+  Flex,
 } from "@chakra-ui/react"
-import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { FiExternalLink } from "react-icons/fi"
 
 const Success = ({ text, readMore, onClick }) => (
-    <Box>
-        <Text>{text}</Text>
-        <Text mt={4}>Read more from OWASP about:</Text>
-        <List>
-            {Array.isArray(readMore) && readMore.map(link => (
-                <ListItem>
-                    <ListIcon asChild><ExternalLinkIcon /></ListIcon>
-                    <Link href={link.url} isExternal={true}>{link.text}</Link>
-                </ListItem>
-            ))}
-        </List>
-        <Button
-            width="full"
-            type="button"
-            mt={4}
-            onClick={onClick}
-            autoFocus={true}
-        >
-            Next
-        </Button>
-    </Box>
+  <Box>
+    <Text>{text}</Text>
+    <Text mt={4}>Read more from OWASP about:</Text>
+    <List.Root listStyleType="none">
+      {Array.isArray(readMore) && readMore.map(link => (
+        <List.Item key={link.url}>
+          <Flex alignItems="center" gap={2}>
+            <FiExternalLink />
+            <Link href={link.url} target="_blank" rel="noopener noreferrer">
+              {link.text}
+            </Link>
+          </Flex>
+        </List.Item>
+      ))}
+    </List.Root>
+    <Button
+      width="full"
+      type="button"
+      mt={4}
+      onClick={onClick}
+      autoFocus={true}
+    >
+      Next
+    </Button>
+  </Box>
 )
 
 export default Success
